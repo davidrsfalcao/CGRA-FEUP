d2e499386a2098283548689fafcbdb5e5dc193f8
davidrsfalcao <davidrsfalcao@gmail.com>
2017-03-20 17:25:18 +0000

    update Plane

--- t4/tp4/Plane.js
+++ t4/tp4/Plane.js
@@ -17,65 +17,102 @@ Plane.prototype.constructor = Plane;
 
 Plane.prototype.initBuffers = function() {
 	/* example for nrDivs = 3 :
-	(numbers represent index of point in array)
-	('x's represent vertices which are drawn but not stored
+	(numbers represent index of point in vertices array)
 
-	y
-	^
-	|
-	0    2    4    6    
-	|
-	1    3    5    7
-	|
-	x	 x	  x    x
-	|
-	x----x----x----x---> x
+	        y
+        	^
+	        |
+	0    1  |  2    3
+	        |
+	4	 5	|  6    7
+	--------|--------------> x
+	8    9  |  10  11
+	        |
+	12  13  |  14  15    
+
 	*/
 
-	// Generate vertices
+	// Generate vertices and normals 
 	this.vertices = [];
-	var xCoord = -0.5;
-	for (var i = 0; i <= this.nrDivs; i++) {
-		this.vertices.push(xCoord, 0.5, 0);
-		this.vertices.push(xCoord, 0.5 - this.patchLength, 0);
-		xCoord += this.patchLength;
+	this.normals = [];
+	
+	// Uncomment below to init texCoords
+	//this.texCoords = [];
+
+	var yCoord = 0.5;
+
+	for (var j = 0; j <= this.nrDivs; j++) 
+	{
+		var xCoord = -0.5;
+		for (var i = 0; i <= this.nrDivs; i++) 
+		{
+			this.vertices.push(xCoord, yCoord, 0);
+			
+			// As this plane is being drawn on the xy plane, the normal to the plane will be along the positive z axis.
+			// So all the vertices will have the same normal, (0, 0, 1).
+			
+			this.normals.push(0,0,1);
+
+			// texCoords should be computed here; uncomment and fill the blanks
+			// this.texCoords.push(..., ...);
+
+			xCoord += this.patchLength;
+		}
+		yCoord -= this.patchLength;
 	}
 	
 	// Generating indices
-	/* for nrDivs = 3 output will be [0, 1, 2, 3, 4, 5, 6, 7].
-	Interpreting this index list as a TRIANGLE_STRIP will draw a row of the plane. */
+	/* for nrDivs = 3 output will be 
+		[
+			 0,  4, 1,  5,  2,  6,  3,  7, 
+			    7,  4,
+			 4,  8, 5,  9,  6, 10,  7, 11,
+			   11,  8,
+			 8, 12, 9, 13, 10, 14, 11, 15,
+		]
+	Interpreting this index list as a TRIANGLE_STRIP will draw rows of the plane (with degenerate triangles in between. */
+
 	this.indices = [];
-	for (var i = 0; i <= 2 * this.nrDivs + 1; i++) {
-		this.indices.push(i);
-	}
+	var ind=0;
 
-	// Generating normals
-	/*
-	As this plane is being drawn on the xy plane, the normal to the plane will be along the positive z axis.
-	So all the vertices will have the same normal, (0, 0, 1).
-	*/
-	this.normals = [];
-	for (var i = 0; i <= 2 * this.nrDivs + 1; i++) {
-		this.normals.push(0,0,1);
-	}
 
+	for (var j = 0; j < this.nrDivs; j++) 
+	{
+		for (var i = 0; i <= this.nrDivs; i++) 
+		{
+			this.indices.push(ind);
+			this.indices.push(ind+this.nrDivs+1);
+
+			ind++;
+		}
+		if (j+1 < this.nrDivs)
+		{
+			// Extra vertices to create degenerate triangles so that the strip can wrap on the next row
+			// degenerate triangles will not generate fragments
+			this.indices.push(ind+this.nrDivs);
+			this.indices.push(ind);
+		}
+	}
+	
 	this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+
+/* Alternative with TRIANGLES instead of TRIANGLE_STRIP. More indices, but no degenerate triangles */
+/*
+	for (var j = 0; j < this.nrDivs; j++) 
+	{
+		for (var i = 0; i < this.nrDivs; i++) 
+		{
+			this.indices.push(ind, ind+this.nrDivs+1, ind+1);
+			this.indices.push(ind+1, ind+this.nrDivs+1, ind+this.nrDivs+2 );
+
+			ind++;
+		}
+		ind++;
+	}
+
+	this.primitiveType = this.scene.gl.TRIANGLES;
+*/
+
 	this.initGLBuffers();
 };
 
-// Drawing the plane
-/*
-To draw the plane we need to draw the row we defined, nrDivs times.
-Each row must be drawn patchLength lower than the one before it.
-To draw each row, the drawElements() function is used. This function draws the geometry defined in initBuffers();
-*/
-Plane.prototype.display = function() {
-	this.scene.pushMatrix();
-
-	for (var i = 0; i < this.nrDivs; i++) {
-		this.drawElements(this.primitiveType);
-		this.scene.translate(0, -this.patchLength, 0);
-	}
-
-	this.scene.popMatrix();
-}
